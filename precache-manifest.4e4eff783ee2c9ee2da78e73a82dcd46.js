b0792216348a17546fb033187c8447af506a988f
BRonen <brennopereira6@gmail.com>
2019-12-20 04:30:04 -0300

    Updates

---
+++ precache-manifest.4e4eff783ee2c9ee2da78e73a82dcd46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda2bb8f58f24ba8841a4ff1c3d14cf4",
+    "url": "/myChat/index.html"
+  },
+  {
+    "revision": "cc992da4366dfe5ec626",
+    "url": "/myChat/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "aef8d8089e236c6a8380",
+    "url": "/myChat/static/js/2.85cf8782.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/myChat/static/js/2.85cf8782.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc992da4366dfe5ec626",
+    "url": "/myChat/static/js/main.ef24767c.chunk.js"
+  },
+  {
+    "revision": "fc1254e5f72a586f81d2",
+    "url": "/myChat/static/js/runtime-main.0f2a7e77.js"
+  }
+]);
